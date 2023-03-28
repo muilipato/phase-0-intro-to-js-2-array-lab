b2085ba40b8c478801843c45c293191367086150
Patrick Maweu <muilipato@gmail.com>
2023-03-28 13:08:10 +0300

    Arrays lab done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats
+}
+
+function appendCat(name){
+    let myArray = [...cats, name];
+    return myArray;
+}
+
+function prependCat(name){
+    let myArray =[name, ...cats]
+    return myArray;
+}
+
+function removeLastCat(){
+    let myArray = [...cats];
+    myArray.pop();
+    return myArray;
+
+}
+ function removeFirstCat(){
+    let myArray =[...cats];
+    myArray.shift();
+    return myArray;
+ }
